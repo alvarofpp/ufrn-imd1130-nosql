a850d636904f8c76b6e3bb1575db3013e936311a
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2019-08-26 10:05:56 -0300

    Realizando as operações de fatorial com cache

--- fatorial-with-cache/src/controllers/FactorialController.js
+++ fatorial-with-cache/src/controllers/FactorialController.js
@@ -1,23 +1,22 @@
 const redisCache = require('../redis/Cache');
 
 
-module.exports.factorial = {
-  /**
-   * Calcula a fatorial.
-   * @param number is a integer
-   * @returns number
-   */
-  factorialCalc: function (number) {
-    return (number === 0)?1:number * factorialCalc(number-1);
-  },
+/**
+ * Calcula a fatorial.
+ * @param number is a integer
+ * @returns number
+ */
+function factorialCalc(number) {
+  return (number === 1)?number:number * factorialCalc(number-1);
+}
 
+module.exports.factorial = {
   /**
    *
    */
   show: async function (req, res) {
     const keyNumber = req.params.keyNumber;
     const key = `factorial:${keyNumber}`;
-    console.log(redisCache);
 
     redisCache.cache.exists(key, (err, reply) => {
       if (reply === 1) {
@@ -25,26 +24,10 @@ module.exports.factorial = {
         redisCache.responseCache(key, res);
       } else {
         console.log('Calculating...');
-        const fat = this.factorialCalc(keyNumber);
+        const fat = factorialCalc(keyNumber);
+        redisCache.updateCache(key, fat, res);
+        console.log(`fat: ${fat}`);
       }
-    });
-  },
-
-  /**
-   *
-   */
-  ler: async function(req, res) {
-    redisCache.cache.get('sorteio:last:megasena', (err, reply) => {
-      res.send((err)?'ERROR':reply);
-    });
-  },
-
-  /**
-   *
-   */
-  escrever: async function (req, res) {
-    redisCache.cache.set('sorteio:last:megasena', '1,2,3,4,5,6', (err, reply) => {
-      res.send((err)?'ERROR':reply);
     });
   }
 };
